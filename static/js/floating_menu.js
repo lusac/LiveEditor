b078a5542135433b2bb8bddec8e9c16fa0d0995e
Lucas Chuairi Cruz <lusacrj@gmail.com>
2016-01-20 11:57:14 -0200

    Method to create items inside floating menu

--- static/js/floating_menu.js
+++ static/js/floating_menu.js
@@ -10,24 +10,32 @@
     FloatingMenu.prototype.init = function (params) {
         this.$menu = $('<ul id="floating-settings" class="dropdown-menu" role="menu">');
         $('body').append(this.$menu);
+        this.bindEvents();
     };
 
     FloatingMenu.prototype.create = function (params) {
         var name = params.name || '(element)',
             posTop = params.posTop || 0,
             posLeft = params.posLeft || 0,
-            menuHtml = '<li role="presentation"><a role="menuitem" tabindex="-1" href="#"> (' + name + ') </a></li>';
-            menuHtml += '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Edit Element</a></li>';
-            menuHtml += '<li role="presentation" class="divider"></li>';
-            menuHtml += '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Move and Resize</a></li>';
-            menuHtml += '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Remove</a></li>';
-            menuHtml += '<li role="presentation" class="divider"></li>';
-            menuHtml += '<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Select Container</a></li>';
+            menuHtml = this.newItem({name: name, is_header: true});
+
+        menuHtml += this.newItem({name: 'Edit Element', operation: 'edit-element'});
+        menuHtml += this.newItem({klass: 'divider'});
+        menuHtml += this.newItem({name: 'Move and Resize', operation: 'move-and-resize'});
+        menuHtml += this.newItem({name: 'Remove', operation: 'remove'});
+        menuHtml += this.newItem({klass: 'divider'});
+        menuHtml += this.newItem({name: 'Select Container', operation: 'select-container'});
 
         this.$menu.append(menuHtml);
         this.$menu.css({
             top: posTop - 10,
             left: posLeft + 10
+        });
+    };
+
+    FloatingMenu.prototype.bindEvents = function () {
+        this.$menu.on('click', 'li', function(e) {
+            console.log('Item clicked');
         });
     };
 
@@ -39,6 +47,27 @@
         this.$menu.hide();
         this.$menu.empty();
     };
+
+    FloatingMenu.prototype.newItem = function (params) {
+        var $li = $('<li role="presentation">');
+
+        if (params.operation) {
+            $li.attr('data-operation', params.operation);
+        }
+
+        if (params.is_header) {
+            $li.addClass('dropdown-header')
+               .append('(' + params.name + ')');
+        } else if (params.name) {
+            $li.append('<a role="menuitem" tabindex="-1" href="#">' + params.name + '</a>');
+        }
+
+        if (params.klass) {
+            $li.addClass(params.klass);
+        }
+
+        return $li.prop('outerHTML');
+    }
 
     window.FloatingMenu = FloatingMenu;
 })(window, document, $);
